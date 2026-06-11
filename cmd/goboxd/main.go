b6da8ac143a85e6d2aa094727f3817a459049797
kripto-geek <geek1998hacker@gmail.com>
2026-06-11 22:18:41 +0530

    v4

--- cmd/goboxd/main.go
+++ cmd/goboxd/main.go
@@ -13,8 +13,9 @@ import (
 )
 
 type Request struct {
-	Source string     `json:"source"`
-	Tests  []TestPart `json:"tests"`
+	Language string     `json:"language"`
+	Source   string     `json:"source"`
+	Tests    []TestPart `json:"tests"`
 }
 
 type TestPart struct {
@@ -29,17 +30,7 @@ func main() {
 	r.Run(":8080")
 }
 
-func RunHandler(c *gin.Context) {
-	var req Request
-	err := c.ShouldBindJSON(&req)
-	if err != nil {
-		c.JSON(400, gin.H{
-			"error": err.Error(),
-		})
-		return
-	}
-	fmt.Println(req.Source)
-
+func RunPythonCode(req Request, c *gin.Context) {
 	dir, err := os.MkdirTemp("", "goboxd-*")
 	if err != nil {
 		return
@@ -85,4 +76,81 @@ func RunHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"result": "Tests Success!",
 	})
+}
+
+func RunCCode(req Request, c *gin.Context) {
+	dir, err := os.MkdirTemp("", "goboxd-*")
+	if err != nil {
+		return
+	}
+
+	defer os.RemoveAll(dir)
+
+	sourcePath := filepath.Join(dir, "sol.c")
+
+	os.WriteFile(sourcePath, []byte(req.Source), 0644)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 7*time.Second)
+
+	cmd := exec.CommandContext(ctx, "gcc", "-o", "sol", "sol.c")
+
+	cmd.Dir = dir
+
+	cmd.CombinedOutput()
+
+	for _, test := range req.Tests {
+
+		cmd := exec.CommandContext(ctx, ".\\sol.exe")
+
+		cmd.Stdin = strings.NewReader(test.Stdin)
+
+		cmd.Dir = dir
+
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			cancel()
+			return
+		}
+		cancel()
+		ActualOutput := strings.TrimSpace(string(output))
+		ExpectedOutput := strings.TrimSpace(string(test.Expected))
+
+		fmt.Print(ActualOutput)
+		fmt.Print(ExpectedOutput)
+
+		if ActualOutput != ExpectedOutput {
+			c.JSON(200, gin.H{
+				"output":   "Wrong Answer",
+				"expected": ExpectedOutput,
+				"acutal":   ActualOutput,
+			})
+			return
+		}
+	}
+	c.JSON(200, gin.H{
+		"result": "Tests Success!",
+	})
+}
+
+func RunHandler(c *gin.Context) {
+	var req Request
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	fmt.Println(req.Source)
+	fmt.Println(req.Language)
+
+	switch req.Language {
+
+	case "python":
+		RunPythonCode(req, c)
+
+	case "C":
+		RunCCode(req, c)
+
+	}
 }
